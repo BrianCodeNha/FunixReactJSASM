e478742e3435c7b14eea26f9321b314b98137721
BrianCodeNha <nmchi1990@gmail.com>
2022-03-10 20:46:53 +0700

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from './components/MenuComponent'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Menu />
+    <App />    
   </React.StrictMode>,
   document.getElementById('root')
 );
