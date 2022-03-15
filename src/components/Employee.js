c5decc56f0805a025d7588720590c709a71d9290
BrianCodeNha <nmchi1990@gmail.com>
2022-03-15 18:29:09 +0700

    hoan thanh employee

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from "react";
+import dateFormat from 'dateformat'
 import {useParams} from 'react-router-dom'
 
 export default function Employee(props) {
+  const id = useParams().staffId;
+  const idList = props.staffs.map((a) => a.id)
+  const index = idList.indexOf(parseInt(id))
+  const staff = props.staffs[index] // có thể dùng trực tiếp id, tuy nhiên do id và index trùng nhau nên mới được, nếu id # index phải phải dùng cách như trên
 
-  const param = useParams()
+  // ngoài ra có thể truyền props từ main component là từng employee, thông qua việc click vào hình, tuy nhiên bắt phải có event click thì mới chạy được, còn nếu reload loại trang sẽ cho ra trang trắng vì không có load data từ event onClick.
+  
 
   return (
-    <div>
-     {param.staffId}
+    <div className="row container">
+      <div className="col-12 col-md-4 col-lg-3">
+        <img src={staff.image} alt={staff.name} />
+      </div>
+      <div className="col-12 col-md-8 col-lg-9">
+        <p>
+          Họ và tên: {staff.name} <br />
+          Ngày sinh: {dateFormat(staff.doB,"dd/mm/yyyy")}<br />
+          Ngày vào công ty: {dateFormat(staff.startDate,"dd/mm/yyyy")}<br />
+          Phòng ban: {staff.department.name}<br />
+          Số ngày nghỉ còn lại: {staff.annualLeave}<br />
+          Số ngày đã làm thêm: {staff.overTime}<br />
+        </p>
+      </div>
     </div>
-  )
+  );
 }
